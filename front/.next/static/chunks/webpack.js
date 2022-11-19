5b99c47d765fc940753a1d2b194896b20f2437fa
Nikita <nikitkavershinin2019@gmail.com>
2022-11-19 18:40:19 +0300

    what is it?

--- front/.next/static/chunks/webpack.js
+++ front/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9e2618d9740fffd7"; }
+/******/ 		__webpack_require__.h = function() { return "155f8940e541d211"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
