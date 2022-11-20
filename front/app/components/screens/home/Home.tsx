8c06e8d8db5e98093afd4fb3043fc2505a1b48a1
Nikita <nikitkavershinin2019@gmail.com>
2022-11-20 14:23:42 +0300

    skeleton in rating

--- front/app/components/screens/home/Home.tsx
+++ front/app/components/screens/home/Home.tsx
@@ -1,14 +1,22 @@
 import { FC } from 'react'
 
-import Layout from '@/components/layout/Layout'
+import Heading from '@/components/ui/heading/Heading'
+
+import Meta from '@/utils/meta/Meta'
 
 import { IHome } from './home.interface'
 
 const Home: FC<IHome> = () => {
 	return (
-		<Layout>
-			<h1 className="text-gray-300">Hello</h1>
-		</Layout>
+		<Meta
+			title="Просмотр фильмов онлайн"
+			description="Просмотр фильмов и телевизионных программ в вашем браузере"
+		>
+			<Heading
+				title="Просмотр фильмов онлайн"
+				className="text-gray-300 mb-8 text-xl"
+			/>
+		</Meta>
 	)
 }
 
