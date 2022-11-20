ea1d77ca49b132f758dcd9f8aa6615f19018b59c
Nikita <nikitkavershinin2019@gmail.com>
2022-11-20 11:21:24 +0300

    why

--- front/.next/static/chunks/webpack.js
+++ front/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "118372de121aca81"; }
+/******/ 		__webpack_require__.h = function() { return "0f0f491eeec773be"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- front/app/components/layout/Navigation/MenuContainer/genres/GenreMenu.tsx
+++ front/app/components/layout/Navigation/MenuContainer/genres/GenreMenu.tsx
@@ -1,8 +1,10 @@
+import { FC } from 'react'
+
 import Menu from '../Menu'
 
 import { usePopularGenres } from './usePopularGenres'
 
-const GenreMenu = () => {
+const GenreMenu: FC = () => {
 	const { isLoading, data } = usePopularGenres()
 	return isLoading ? (
 		<div className="mx-11 mb-6">Loading...</div>
